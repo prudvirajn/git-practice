80be2dbc22782dbb34978fde117ce5d251d5425c
Rajprudvin <prudvirajn@gmail.com>
2024-08-26 16:33:35 +0530

    conversations

---
+++ 02-conversations.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh::hi Suresh. how are you?"
+echo "Suresh::hello Ramesh iam fine.how are you doing?"
+echo "Ramesh::im good Suresh what is going on?"
+echo "suresh::i started practice shell script Ramesh"
