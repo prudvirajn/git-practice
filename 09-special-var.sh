38fd6abfcf4d852bde242b6b24e52a7b7faf000b
Rajprudvin <prudvirajn@gmail.com>
2024-08-27 17:09:54 +0530

    data types

--- 09-special-var.sh
+++ 09-special-var.sh
@@ -6,3 +6,4 @@ echo "script name: $0"
 echo "current working directory: $PWD"
 echo " process idof script execute now: $$"
 echo "home directory of current user: $HOME"
+echo ""last background pid: $!
